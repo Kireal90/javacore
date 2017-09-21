f43ea1154accf874f0b6f07e8359a857c3f2c485
Kirill Belevets <kirill.belevets@gmail.com>
2017-09-21 17:25:54 +0300

    Uncommented MathFuncTest class

--- src/test/java/com/kitcenter/app/classwork/MathFuncTest.java
+++ src/test/java/com/kitcenter/app/classwork/MathFuncTest.java
@@ -10,14 +10,14 @@ import org.junit.runner.RunWith;
 
 public class MathFuncTest {
 
-//    @RunWith(JUnitParamsRunner.class)
-//    public class DataProviderMathFunctionsTest {
-//        @Test
-//        @FileParameters(value = "src/test/resources/testdata.csv", mapper = CsvWithHeaderMapper.class)
-//        public void dataProviderTestAdd (int argA, int argB, int expOut) {
-//            int actOut = Math.multiplyExact(argA, argB);
-//            Assert.assertEquals(expOut, actOut);
-//        }
-//    }
+    @RunWith(JUnitParamsRunner.class)
+    public class DataProviderMathFunctionsTest {
+        @Test
+        @FileParameters(value = "src/test/resources/testdata.csv", mapper = CsvWithHeaderMapper.class)
+        public void dataProviderTestAdd (int argA, int argB, int expOut) {
+            int actOut = Math.multiplyExact(argA, argB);
+            Assert.assertEquals(expOut, actOut);
+        }
+    }
 
 }
